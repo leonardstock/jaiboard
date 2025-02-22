8857cfb66d18c7f2b8961aabadccbd6678b59aa6
Leo Stock <leo.stock@live.de>
2025-02-22 09:56:51 +0000

    changed apollo url to env variable

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -8,7 +8,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject> | null = null;
 
 const createApolloClient = () =>
     new ApolloClient({
-        uri: "http://localhost:3000/api/graphql", // App Router API path
+        uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
         cache: new InMemoryCache(),
     });
 
